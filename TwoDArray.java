a7176c8d372644104067df372295f1cd9afd6ee0
Gopal kumar Das <35631909+Gopaldas5@users.noreply.github.com>
2022-05-25 20:06:57 +0530

    Update TwoDArray.java

--- TwoDArray.java
+++ TwoDArray.java
@@ -10,16 +10,16 @@ public class TwoDArray {
     int m = sc.nextInt();
     System.out.print("Enter the length of  N cons:- ");
     int n = sc.nextInt();
-    int a[][] = new int[m][n];//declaration
+    int a[][] = new int[m][n];
     System.out.print("Enter "+m*n +" Elements to Store in Array :\n");
-      for( int i = 0; i<m;i++)
+      for( int i = 0; i< m; i++)
        {
         for (int j = 0; j < n; j++) {
             a[i][j] = sc.nextInt();
           }
        }
         System.out.print("Elements in Array are :\n");
-          for(int i = 0;i<m;i++)
+          for(int i = 0; i < m; i++)
           {
         for (int j = 0; j < n; j++) {
             System.out.println("Row [" + i + "]:  Column [" + j + "] :" + a[i][j]);
