d92971692699a955552bcf4a433c8eecdbde1c47
Gopal kumar Das <35631909+Gopaldas5@users.noreply.github.com>
2022-07-04 08:53:09 +0530

    Update Distance.java

--- Distance.java
+++ Distance.java
@@ -9,12 +9,12 @@ public class Distance {
         int x1,y1;
         double dis;
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter x1 point:- ");
-        x1=sc.nextInt();
-        System.out.println("enter y1 point:- ");
-        y1=sc.nextInt();
-        dis= Math.sqrt(Math.pow(x1, 2) + Math.pow(y1, 2));
-        System.out.println("Distance between to point:- "+"("+x1+","+y1+") is:-  "+dis);
+        System.out.println("Enter x1 point :- ");
+        x1 = sc.nextInt();
+        System.out.println("Enter y1 point :- ");
+        y1 = sc.nextInt();
+        dis = Math.sqrt(Math.pow(x1, 2) + Math.pow(y1, 2));
+        System.out.println("Distance between to point:- "+"(" +x1+ "," +y1+ ") is :-  "+dis);
 
     }
 }
